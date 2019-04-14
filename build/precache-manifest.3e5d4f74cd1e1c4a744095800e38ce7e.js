2ff0b7962efde55c839c952056c56b3e5201acd1
BacchusKC <bacchuskc@gmail.com>
2019-04-14 01:50:37 -0500

    build

---
+++ build/precache-manifest.3e5d4f74cd1e1c4a744095800e38ce7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b36acce4c2ca65ca25626437c2d7983",
+    "url": "/static/media/background.4b36acce.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3371f4eb82e0329984bc",
+    "url": "/static/js/main.e11a0889.chunk.js"
+  },
+  {
+    "revision": "c65f0670c7d7a243f52e",
+    "url": "/static/js/2.f75e8bbc.chunk.js"
+  },
+  {
+    "revision": "3371f4eb82e0329984bc",
+    "url": "/static/css/main.89e7a4d7.chunk.css"
+  },
+  {
+    "revision": "d1e64db04e54f3cede8e248d42bfdd44",
+    "url": "/index.html"
+  }
+];
