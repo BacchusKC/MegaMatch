d9e95202b20476d4fa998da03aea1f6e65382f29
BacchusKC <bacchuskc@gmail.com>
2019-04-14 02:08:55 -0500

    Updates

---
+++ precache-manifest.e8b0456d41e62c4b43d2d6373afe9535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b36acce4c2ca65ca25626437c2d7983",
+    "url": "/MegaMatch/static/media/background.4b36acce.jpg"
+  },
+  {
+    "revision": "7891f636eb6efd1e1943",
+    "url": "/MegaMatch/static/js/runtime~main.ffb54604.js"
+  },
+  {
+    "revision": "6b94da3f6b78dbb9bdcb",
+    "url": "/MegaMatch/static/js/main.f4fd9b84.chunk.js"
+  },
+  {
+    "revision": "c65f0670c7d7a243f52e",
+    "url": "/MegaMatch/static/js/2.f75e8bbc.chunk.js"
+  },
+  {
+    "revision": "6b94da3f6b78dbb9bdcb",
+    "url": "/MegaMatch/static/css/main.eb519bf8.chunk.css"
+  },
+  {
+    "revision": "df2d34c56eaa5eea4af5f7a07f02a48e",
+    "url": "/MegaMatch/index.html"
+  }
+];
